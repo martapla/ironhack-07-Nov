a2ae4aae682a675be6c72b230d93e26383c5bb42
martapla <martaplanella@hotmail.com>
2022-11-14 22:26:30 +0100

    section-3-js

---
+++ section-3-js/exercise data.js
@@ -0,0 +1,62 @@
+
+//Object 1
+let bio = {
+    firstName: "Marta",
+    lastName: "Pla",
+    cities: ["Barcelona", "London"],
+    age: 60,
+    dob: "07/05/1960",
+    drinkAlcohol: true,
+
+}
+//console.log (`Users drink alcohol: ${bio.drinkAlcohol} `)
+
+//Object 2
+
+let books = [
+    {
+        bookName: "Tao Te Ching",
+        genre: "Poetry",
+        author: "Lao Tse",
+        year: 00,
+        ISBN: 97884206434,
+    },
+    {
+        bookName: "La vieja sirena",
+        genre: "novel",
+        author: "Jose Luis Sampedro",
+        year: 1990,
+        ISBN: 7353485858,
+    },
+    {
+        bookName: "Elogio del ingenio",
+        genre: "Novel",
+        author: "Jose Antonio Marina",
+        year: 1992,
+        ISBN: 83546575858,
+    },
+];
+
+//console.log(`My favourite book is: ${books[2].bookName}`);
+
+
+const firstName = "Marta";
+const lastName = "Pla";
+const age = 60;
+let hobbie = "sewing";
+ 
+console.log(`My name is ${firstName} , I'm ${bio.age} and I like ${hobbie} `);
+
+ hobbie = "walking my dog";
+console.log(`My name is ${firstName} , I'm ${bio.age} and I like ${hobbie} `);
+
+ hobbie = "going to the mountain";
+console.log(`My name is ${firstName} , I'm ${bio.age} and I like ${hobbie} `);
+
+ hobbie = "cooking";
+console.log(`My name is ${firstName} , I'm ${bio.age} and I like ${hobbie} `);
+
+ hobbie = "watching films";
+console.log(`My name is ${firstName} , I'm ${bio.age} and I like ${hobbie} `);
+
+
