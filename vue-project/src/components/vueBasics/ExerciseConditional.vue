f5b435880927101ad56306a329cb38196714a0b7
martapla <martaplanella@hotmail.com>
2022-11-29 21:33:02 +0100

    vue day 2

---
+++ vue-project/src/components/vueBasics/ExerciseConditional.vue
@@ -0,0 +1,91 @@
+<!-- // Ej.1: Creamos dos <h1> diferentes, uno que diga "I'm showing up beacuse our reactive data is true!" y el otro que diga "I'm the option anytime our reactive data cannot be read as true". Como vemos, los dos dependerán de una variable reactiva en vuestro script y, según si esta es true or false, enseñaréis uno u otro. -->
+
+ <!-- // Ej.2: Creamos cuatro <p> tags. Los cuatro se renderizarán condicionalmente mirando a una misma variable que llamaremos "language". Esta variable podrá tener tres valores distintos: "HTML", "CSS" o "JS". En nuestro template deberemos leer dicha variable y enseñar un <p> u otro según su valor. Cada uno de los <p> tags a su vez contendrá el texto "Today we are learing" seguido del valor de la variable que imprimiréis en pantalla usando la moustache syntax. El último <p> tag se encargará de aparecer cuando el valor de "language" no sea ninguno de los tres mencionado anteriormente, y contendrá el texto "Today there's no class!". -->
+
+ <!-- Ej.3***: Creamos una variable reactiva que contenga un objeto con vuestro nombre (string), vuestra edad (number) y una key hasCodedBefore (boolean) la cual será true or false dependiendo de si antes del bootcamp habíais picado código o no. Después creamos otra variable reactiva booleánica que podéis llamar show, por ejemplo. Según si show está en true or false, en vuestro template deberíais enseñar o no una pequeña biografía. Esta biografía, a su vez, constará primero de vuestro nombre como título. Después tendrá un <p> que se renderizará de manera condicional. En caso de que vuestra edad sea superior a 18 enseñaremos la frase "I am an adult! :)"; en caso contrario, enseñaremos "I'm still a minor :(". Finalmente, según si hasCodedBefore es true or false también renderizaréis "I am still a newbie to code but I'm learning fast" o "This is not my first time at the javascript rodeo". -->
+
+
+<template>
+    <div id="class-excercises">
+    <div id="ejercicio1">
+      <h2>Ejercicio 01</h2>
+      <!-- Reactive Data Type -->
+      <h1 v-if="ejercicio1.isTrue === false">
+        I'm showing up beacuse our reactive data is true!
+      </h1>
+      <h1 v-else>
+        I'm the option anytime our reactive data cannot be read as true
+      </h1>
+      <!-- Ref Data Type -->
+      <h1 v-if="ejemplo2">I'm showing up beacuse our reactive data is true!</h1>
+      <h1 v-else>
+        I'm the option anytime our reactive data cannot be read as true
+      </h1>
+    </div>
+    <div id="ejercicio2">
+      <h2>Ejercicio 02</h2>
+      <p v-if="language === 'HTML'">Today we are learning {{ language }}</p>
+      <p v-else-if="language === 'CSS'">Today we are learning {{ language }}</p>
+      <p v-else-if="language === 'JS'">Today we are learning {{ language }}</p>
+      <p v-else>Today there's no class just WU TANG PARTY!</p>
+    </div>
+    <div id="ejercicio3">
+      <h2>Ejercicio 03</h2>
+      <!-- Example 001 - reactive data containing the boolean -->
+      <div v-if="bio.show">
+        <h1>{{ bio.name }}</h1>
+        <p v-if="bio.age > 18">I am an adult!</p>
+        <p v-else>I'm still a young git!</p>
+
+        <p v-if="bio.hasCodeBefore">
+          YES LAWD, This is not my first time at the javascript rodeo
+        </p>
+        <p v-else>I am still a newbie to code but I'm learning fast</p>
+      </div>
+      <div v-else>Yo go get famous cause there's no bio for you!</div>
+      <!-- Example 002 - ref data containing the boolean -->
+      <div v-if="show">
+        <h1>{{ bio.name }}</h1>
+        <p v-if="bio.age > 18">I am an adult!</p>
+        <p v-else>I'm still a young git!</p>
+
+        <p v-if="bio.hasCodeBefore">
+          YES LAWD, This is not my first time at the javascript rodeo
+        </p>
+        <p v-else>I am still a newbie to code but I'm learning fast</p>
+      </div>
+      <div v-else>Yo go get famous cause there's no bio for you!</div>
+    </div>
+  </div>
+
+</template>
+
+
+
+<script>
+import { ref,reactive } from "vue";
+
+// Ejercicio 001
+const ejercicio1 = reactive({
+  isTrue: true,
+  isFalse: false,
+});
+const ejemplo2 = ref(false);
+// Ejercicio 002
+const language = ref("CSS");
+//EJercicio 003
+const bio = reactive({
+  name: "Anderson Paak",
+  age: 41,
+  hasCodeBefore: false,
+  show: false,
+});
+const show = ref(true);
+
+
+   
+</script>
+
+<style>
+
+</style>
---
+++ vue-project/src/components/vueBasics/ExerciseIterating.vue
@@ -0,0 +1,61 @@
+<!-- // Ejercicios Iterating Elements
+// Creamos la estructura básica de un componente de vue en la que vamos a mostrar el resultado de los ejercicios. Recordad importar el componente donde sea necesario para poder ver el avance de vuestros ejercicios.
+// Ej.1: Creamos una variable reactiva que contenga un array con vuestras cinco películas preferidas. En el template pondremos un título de My favorite Movies y después una <ol>. Dentro de la ordered list recorreremos el array de películas y crearemos un <li> para cada una que nos las printee en pantalla. A cada <li> le daremos el index del elemento en el array como key.
+
+// Ej.2:  Creamos una variable reactiva que contenga un objeto con cinco keys: favoriteFood, favoriteDrink, favoriteSport, favoriteCountry y favoriteArrayMethod. En el template vamos a recorrer el objeto para mostrar cada key y su valor en una unoredered list (en pantalla deberíamos ver algo como "favoriteFood: Sushi").
+
+// Ej.3***: Creamos una variable reactiva students que contenga un array con al menos tres objetos. Cada objeto representará uno de vuestros compañeros y tendrá las keys name (string), age(number) y favoriteLanguages(array de strings). Tendremos que recorrer el array en el template y para cada objeto printear el nombre de vuestro compañero. Según si su edad es mayor o menor a 18 printearemos "I can go for a few drinks with NAME after class" o "NAME is still too young, they'll have to go home tonight". Finalmente, loopeamos el array de sus lenguajes de programación favoritos dentro de una unordered list. -->
+
+<template>
+
+  <h1>My favourite movies</h1>
+  <div class="ex1">
+    <ol>
+    
+        <li v-for="(peli,index) in pelis" :key="index">
+        {{index + 1}} - {{peli}}
+        </li>
+
+    </ol>
+    <div class="ex2">
+        <ul>
+            <li v-for="(favourite,index) in favs" :key="index">
+            <div>
+                <h4> Favourite food: {{favoriteFood}}</h4>
+                <h4> Favourite drink: {{favoriteDrink}}</h4>
+            </div>
+            </li>
+        </ul>
+
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+// Ex.1
+const pelis = reactive([
+    "There will be blood",
+    "The Last of the Mohicans",
+    "Gangs of New York",
+    "The Devil wears prada",
+    "Kramer vs Kramer"
+])
+
+//EX.2
+const favs = reactive([
+     favoriteFood = "Soup" ,
+     favoriteDrink = "Tea" ,
+     favoriteSport = "Ioga" ,
+     favoriteCountry = "Bcn" ,
+     favoriteArrayMethod = "sort",
+])
+
+//Ex.3
+
+
+</script>
+
+<style>
+
+</style>
